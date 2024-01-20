4716d445272d5c4a16ffd6a9cf86c6b583853fe0
Egide Hirwa <66557478+egidehirwa@users.noreply.github.com>
2024-01-20 23:57:31 +0100

    reviewed while loop, method chaining

--- index.js
+++ index.js
@@ -442,6 +442,7 @@ console.log(fullName.indexOf(" "))
 
 ///////////////////////////////////////
 
+/* 
 const email = "egidius@gmail.com"
 
 let userName = email.slice(0, email.indexOf("@"));
@@ -449,3 +450,94 @@ let extension = email.slice(email.indexOf("@") + 1)
 
 console.log(userName);
 console.log(extension);
+ */
+
+// Method chaining
+
+/* let username = window.prompt("Enter your username: ");
+
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+
+console.log(username) */
+
+// Logical operators
+
+/* 
+const temp = 40;
+
+if(temp <= 0 || temp > 30){
+    console.log("The weather is BAD");
+}else {
+    console.log("The weather is good");
+}
+
+console.log(temp)
+ */
+
+// assignment operator
+// == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values & datatype are equal)
+// != inequality operator
+// !== strict inequality operator
+
+
+// While loop
+
+/* 
+let loggedIn = false;
+let username;
+let password;
+
+while(!loggedIn){
+    username = window.prompt(`Enter your username`);
+    password = window.prompt(`Enter your password`);
+
+    if(username === "myUsername" && password === "myPassword"){
+        loggedIn = true;
+        console.log("You are logged in!")
+    } else {
+        console.log("Wrong credentials")
+    }
+}
+
+ */
+
+// for loop = repeat some code a LIMITED amount of times.
+
+/* 
+for(let i = 1; i <= 20; i++){
+    
+    if(i == 13){
+        break;
+    } else {
+        console.log(i)
+    }
+}
+
+console.log("Happy New Year");
+ */
+
+
+// Number guessing game
+
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}`)
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    } else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number");
+    }
+
+    running = false;
+}
